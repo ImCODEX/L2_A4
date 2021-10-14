d2329c8220042d17c886132127e6cc014b05b378
Razvan Postescu <razvanpostescu@gmail.com>
2021-10-14 14:58:55 +0300

    Added max method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,10 @@ public class Main {
     public static void main(String[] args) {
         Methods m = new Methods();
         int[] kb_prices = new int[]{40, 35, 70, 15, 45};
+        int[] usb_prices = new int[]{20, 15, 40, 15};
 
         System.out.println(m.min_kb(kb_prices));
+        System.out.println(m.max_price(kb_prices,usb_prices));
+
     }
 }
--- src/com/company/Methods.java
+++ src/com/company/Methods.java
@@ -15,4 +15,25 @@ public class Methods {
         }
         return min;
     }
+
+    /**
+     * Maximum of two arrays
+     * @param kb: keyboard prices
+     * @param usb: usb prices
+     * @return: maximum price
+     */
+    public int max_price(int[] kb, int[] usb){
+        int max = 0;
+        for (int j : kb) {
+            if (j > max)
+                max = j;
+        }
+
+        for (int j : usb) {
+            if (j > max)
+                max = j;
+        }
+
+        return max;
+    }
 }
